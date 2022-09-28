d7a39a9868085143fc34250036bebb9cbd48dfc0
angelbravin <angelbravin6@gmail.com>
2022-09-28 02:05:47 -0800

    tried

---
+++ breakandcontinue/continue.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main ()
+{
+
+        int A,num,sub=0;
+        for(A=1;A<=5;++A)
+        {
+                printf ("enter a number:");
+                        scanf("%d",&num);
+
+                        if (num>0)
+                        {
+                                continue;
+                        }
+        }
+                sub-=num;
+
+{
+printf("sub=%d",sub);
+}
+return 0;
+}
+
